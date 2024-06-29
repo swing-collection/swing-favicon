ac7ba3ab0e17a21764ab3ee15326b319e708a7c7
Lars van Vianen <lars@vianen.com>
2024-06-29 10:27:26 +0200

    Create urls.py

---
+++ src/swing-icon/temp/favicon1/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path
+from django.views.generic import RedirectView
+from . import conf
+
+urlpatterns = [
+    re_path(
+        r'^favicon\.ico$',
+        RedirectView.as_view(url=conf.FAVICON_PATH, permanent=True),
+        name='favicon'
+    ),
+]
