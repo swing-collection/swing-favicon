9366a35a4dd0b96559d80b6a730b56618f7535bc
Lars van Vianen <lars@vianen.com>
2024-08-11 09:32:20 +0200

    Create model_favicon.py

---
+++ src/swing_favicon/models/model_favicon.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Provides Favicon Model Class
+============================
+
+
+"""
+
+
+# =============================================================================
+# Import
+# =============================================================================
+
+# Import | Standard Library
+from typing import Any, Dict, List
+
+# Import | Libraries
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+# Import | Local Modules
+
+
+# =============================================================================
+# Classes
+# =============================================================================
+
+class FaviconModel(BaseModel):
+    """
+    Favicon Model Class
+    ===================
+
+        """
+
+    image = models.ImageField(upload_to='favicons/')
+
+    def __str__(self):
+        """
+        """
+        return "Favicon"
+
+# =============================================================================
+# Module Exports
+# =============================================================================
+
+__all__ = [
+    "FaviconModel",
+]
