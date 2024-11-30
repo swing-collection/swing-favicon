470215946f96ea4ed68a53818a009574b3b884cb
Lars van Vianen <lars@vianen.com>
2024-11-30 17:30:29 +0100

    Update view_favicon.py

--- src/swing_favicon/views/view_favicon.py
+++ src/swing_favicon/views/view_favicon.py
@@ -1,13 +1,41 @@
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# Docstring
+# =============================================================================
+
+"""
+Provides Typology Group Model Class
+===================================
+
+
+"""  # noqa E501
+
+
+# =============================================================================
+# Import
+# =============================================================================
+
+# Import | Standard Library
+from typing import Any
+
+# Import | Libraries
 from django.conf import settings
 from django.http import FileResponse, HttpRequest, HttpResponse
 from django.views.decorators.cache import cache_control
 from django.views.decorators.http import require_GET
+
+# Import | Local Modules
 from ..conf import (
     FAVICON_CACHE_MAX_AGE,
     FAVICON_CACHE_IMMUTABLE,
     FAVICON_CACHE_PUBLIC
 )
 
+# =============================================================================
+# Classes
+# =============================================================================
 
 @require_GET
 @cache_control(
@@ -15,6 +43,7 @@ from ..conf import (
     immutable = FAVICON_CACHE_IMMUTABLE,
     public = FAVICON_CACHE_PUBLIC
 )
+
 def favicon(request: HttpRequest) -> HttpResponse:
     """
     """
